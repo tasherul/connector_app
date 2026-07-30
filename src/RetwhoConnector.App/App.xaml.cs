fa82861e7d34a612e918a7edc050888db7324205
Tasherul Islam <47455913+tasherul@users.noreply.github.com>
2026-07-31 03:45:13 +0600

    chore: establish Retwho Connector solution

---
+++ src/RetwhoConnector.App/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RetwhoConnector.App;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
